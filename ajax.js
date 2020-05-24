86ed384ef7b6ec179d58d455b9f34c37d9fc4e29
Jose_Martinez <martinez022jose@gmail.com>
2020-05-24 18:17:53 -0300

    Se arregla funcionamiento correcto formulario-ajax

--- ajax.js
+++ ajax.js
@@ -1,18 +1,18 @@
-var nombre = document.getElementById('nombre').value;
-var apellido = document.getElementById('apellido').value;
-var email = document.getElementById('email').value;
-var mensaje = document.getElementById('mensaje').value;
-var botonEnviarFormulario = document.getElementById('buttonEnviar');
+var botonEnviarFormulario = document.getElementById("buttonEnviar");
 var resFormulario = document.querySelector('section .form .cajaFormulario .formulario .resFormulario');
 
 
 botonEnviarFormulario.addEventListener('click',validarEnvioMail);
 
 function validarEnvioMail(){
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var email = document.getElementById("email").value;
+    var mensaje = document.getElementById("mensaje").value;
+    var datosAEnviar='nombre='+nombre+'&apellido='+apellido+'&email='+email+'&mensaje='+mensaje;
     resFormulario.innerHTML = '';
     var xhttp = new XMLHttpRequest();
     xhttp.open("POST","validacionEnvioMail.php",true);
-    var informacionTotal = "nombre"+nombre+"&apellido"+apellido+"&email"+email+"&mensaje"+mensaje;
     xhttp.onreadystatechange = function(){
         if(xhttp.status == 200 && xhttp.readyState == 4){
             var mensaje = xhttp.responseText;
@@ -21,7 +21,8 @@ function validarEnvioMail(){
         }
 
     }
-    xhttp.send(informacionTotal);
-
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send(datosAEnviar);
     
+
 }
--- validacionEnvioMail.php
+++ validacionEnvioMail.php
@@ -1,39 +1,69 @@
 <?php
-
-
 function validarEspaciosEnBlanco(){
-  if(empty($_POST['nombre']) || empty($_POST['apellido']) || empty( $_POST['email']) || empty($_POST['mensaje'])){
-    return true;
-  }
-  return false;
-
+    if(empty($_POST['nombre']) || empty($_POST['apellido']) || empty($_POST['email']) || empty($_POST['mensaje'])){
+        return true;
+    }
+    return false;
+    
 }
 
-if(validarEspaciosEnBlanco()){
-  echo "<p class='error'>Debe completar todos los campos</p>";
-}else if(strlen($_POST['nombre'])>25){
-    echo "<p class='error'>Error debe ingresar un nombre menor a 25 caracteres</p>";
-    }else if(strlen($_POST['apellido'])>25){
-      echo "<p class='error'>*Error debe ingresar un apellido menor a 25 caracteres</p>";
+function obtenerValores(&$nombre,&$apellido,&$email,&$mensaje){
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $email = $_POST['email'];
+    $mensaje = $_POST['mensaje'];
+}
 
-    }else if(!filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)){
-      echo "<p class='error'>*Error debe ingresar un correo valido</p>";
+function recorrerElementos($elementos){
+    for($i=0;$i<count($elementos);$i++){
+        echo $elementos[$i];
     }
-else{
-      $nombre = $_POST['nombre'];
-      $apellido = $_POST['apellido'];
-      $email = $_POST['email'];
-      $mensaje = $_POST['mensaje'];
-      
-      $destinatario = "martinez022jose@gmail.com";
-      $asunto = "contacto desde nuestra web";
-      $carta = "De: $nombre \n";
-      $carta.= "Correo: $email \n";
-      $carta.= "Mensaje: $mensaje";
+}
 
-     mail($destinatario,$asunto,$carta);
+$errores = Array();
+$mensajeExitoso = Array();
+$nombre="";
+$apellido="";
+$email="";
+$mensaje="";
 
-     echo "<p class='mensajeExitoso'>Mail enviado exitosamente</p>";
+obtenerValores($nombre,$apellido,$email,$mensaje);
+
+if(validarEspaciosEnBlanco()){
+        array_push($errores,"<p class='error'>Se debe completar todos los campos</p>");
+        
+}else{
+    if(strlen($nombre)>25){
+        array_push($errores,"<p class='error'>Error debe ingresar un nombre menor a 25 caracteres</p>");
+        }
+    if(strlen($apellido)>25){
+        array_push($errores,"<p class='error'>Error debe ingresar un apellido menor a 25 caracteres</p>");
+        }
+    if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+        array_push($errores,"<p class='error'>Error debe ingresar un correo valido</p>");
+}}
+
+
+
+    $destinatario = "martinez022jose@gmail.com";
+    $asunto = "contacto desde nuestra web";
+    $carta = "De:".$nombre;
+    $carta.= "Correo".$email;
+    $carta.= "Mensaje:".$mensaje;
+
+   //Verificamos si hubo errores
+
+   if(count($errores)>0){
+        recorrerElementos($errores);
+        
+
+    }else{
+        array_push($mensajeExitoso,"<p class='mensajeExitoso'>Mensaje enviado exitosamente</p>");}
+
+    if(count($mensajeExitoso)>0){
+        recorrerElementos($mensajeExitoso);
+        mail($destinatario,$asunto,$carta);
+    
 
     }
 
